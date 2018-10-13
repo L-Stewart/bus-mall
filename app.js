e7c2db144e8f16167509f58a7a8bd61c6304782a
LorinStewart <stewartalorin@gmail.com>
2018-10-13 10:19:22 -0700

    Added the images

--- app.js
+++ app.js
@@ -3,12 +3,15 @@
 // global var
 // Track what they will be clicking on/ what will have events tied to
 var imageLeft = document.getElementById('left');
+var imageMiddle = document.getElementById('middle');
 var imageRight = document.getElementById('right');
 var imageSelector = document.getElementById('click-me');
 var leftImageText = document.getElementById('left-text');
+var middleImageText = document.getElementById('middle-text')
 var rightImageText = document.getElementById('right-text');
 var currentLeftImageArrayIndex = 0;
-var currentRightImageArrayIndex = 1;
+var currentMiddleImageArrayIndex = 1;
+var currentRightImageArrayIndex = 2;
 var clickCount = 0;
 
 
@@ -16,7 +19,7 @@ var clickCount = 0;
 var allImages = [];
 
 // Image constructor
-var Images = function(src, name){
+var BusMallImages = function(src, name){
   this.src = src;
   this.name = name;
   this.appeared = 0;
@@ -25,57 +28,90 @@ var Images = function(src, name){
   allImages.push(this);
 };
 
+new BusMallImages('/img/bag.jpg', 'R2D2 Roller Bag');
+new BusMallImages('/img/banana.jpg', 'Banana Slicer');
+new BusMallImages('/img/bathroom.jpg', 'Bathroom TP and Tablet Holder');
+new BusMallImages('/img/boots.jpg', 'Boot Sandles');
+new BusMallImages('/img/breakfast.jpg', 'Toaster Oven Coffee Combo');
+new BusMallImages('/img/bubblegum.jpg', 'Bubblegum');
+new BusMallImages('/img/chair.jpg', 'Chair');
+new BusMallImages('/img/cthulhu.jpg', 'Demon');
+new BusMallImages('/img/dog-duck.jpg', 'Best Friend');
+new BusMallImages('/img/dragon.jpg', 'Stew');
+new BusMallImages('/img/pen.jpg', 'Best Pen Caps');
+new BusMallImages('/img/pet-sweep.jpg', 'Entertainment');
+new BusMallImages('/img/scissors.jpg', 'Pizza Scissors');
+new BusMallImages('/img/shark.jpg', 'Shark Sleeping Bag');
+new BusMallImages('/img/sweep.jpg', 'Baby Cleaning Service');
+new BusMallImages('/img/tauntaun.jpg', 'Correct Childhood');
+new BusMallImages('/img/unicorn.jpg', 'Rainbow Stew');
+new BusMallImages('/img/usb.gif', 'Theres a snake in by boot!');
+new BusMallImages('/img/wine-glass.jpg', 'AA Aid');
+
+
 // prototypes
-Images.prototype.renderImage = function(){
+BusMallImages.prototype.renderImage = function(){
   imageLeft.src = this.src;
+  imageMiddle = this.src;
+  imageRight = this.src;
 };
 
-// event listner and handler
-var imageClickHandler = function(event){
-  //when i get the event back
-  if(event.target.id === 'left' || event.target.id === 'right'){
-
-    do {
-      var randomNumberLeft = Math.floor(Math.random() * allImages.length);
-    } while (randomNumberLeft === currentLeftImageArrayIndex || randomNumberLeft === currentRightImageArrayIndex);
-
-    do {
-      var randomNumberRight = Math.floor(Math.random() * allImages.length);
-    } while (randomNumberRight === currentLeftImageArrayIndex || randomNumberRight === randomNumberLeft || randomNumberRight === currentRightImageArrayIndex);
-
-    if(event.target.id === 'left'){
-      allImages[currentLeftImageArrayIndex].likes++;
-      console.log('left was clicked');
-    } else{
-      allImages[currentRightImageArrayIndex].likes++;
-      console.log('right was clicked');
-    }
-
-    allImages[currentLeftImageArrayIndex].appeared++;
-    allImages[currentRightImageArrayIndex].appeared++;
-
-    currentLeftImageArrayIndex = randomNumberLeft;
-    currentRightImageArrayIndex = randomNumberRight;
-
-    // event.target.src = allImages[randomNumber].src;
-    imageLeft = allImages[randomNumberLeft].src;
-    imageRight = allImages[randomNumberRight].src;
-    leftImageText.textContent = allImages[randomNumberLeft].name;
-    rightImageText.textContent = allImages[randomNumberLeft].name;
-
-    clickCount++;
-    if(clickCount === 15){ //tells use we have clicked 15 times
-      renderChart();
 
 
-      //stop listening
-      imageSection.removeEventListener('click', imageClickHandler);
-    }
-  }
-};
+// // event listner and handler
+// var imageClickHandler = function(event){
+//   //when i get the event back
+//   if(event.target.id === 'left' || event.target.id === 'right' || event.target.id === 'middle'){
 
-imageSection.addEventListener('click', imageClickHandler);
+//     do {
+//       var randomNumberLeft = Math.floor(Math.random() * allImages.length);
+//     } while (randomNumberLeft === currentLeftImageArrayIndex || randomNumberLeft === currentRightImageArrayIndex);
 
+//     do {
+//       var randomNumberMiddle = Math.floor(Math.random() * allImages.length);
+//     } while (randomNumberLeft === currentLeftImageArrayIndex || randomNumberLeft === currentRightImageArrayIndex);
+
+//     do {
+//       var randomNumberRight = Math.floor(Math.random() * allImages.length);
+//     } while (randomNumberRight === currentLeftImageArrayIndex || randomNumberRight === randomNumberLeft || randomNumberRight === currentRightImageArrayIndex);
+
+//     if(event.target.id === 'left'){
+//       allImages[currentLeftImageArrayIndex].likes++;
+//       console.log('left was clicked');
+//     } else{
+//       allImages[currentRightImageArrayIndex].likes++;
+//       console.log('right was clicked');
+//     }
+
+//     allImages[currentLeftImageArrayIndex].appeared++;
+//     allImages[currentRightImageArrayIndex].appeared++;
+
+//     currentLeftImageArrayIndex = randomNumberLeft;
+//     currentRightImageArrayIndex = randomNumberRight;
+
+//     // event.target.src = allImages[randomNumber].src;
+//     imageLeft = allImages[randomNumberLeft].src;
+//     imageMiddle = allImages[randomNumberMiddle].src;
+//     imageRight = allImages[randomNumberRight].src;
+//     leftImageText.textContent = allImages[randomNumberLeft].name;
+//     middleImageText.textContent = allImages[randomNumberMiddle].name;
+//     rightImageText.textContent = allImages[randomNumberLeft].name;
+
+//     clickCount++;
+//     if(clickCount === 15){ //tells use we have clicked 15 times
+//       renderChart();
+
+
+//       //stop listening
+//       imageSelector.removeEventListener('click', imageClickHandler);
+//     }
+//   }
+// };
+
+// imageSelector.addEventListener('click', imageClickHandler);
+
+
+//=========================================================================================================================
 // var imageClickHandler = function(event){
 //     do {
 //         var randomNumber = Math.floor(Math.random() * allImages.length);
@@ -116,63 +152,63 @@ imageSection.addEventListener('click', imageClickHandler);
 
 
 
-var renderChart = function(){
+// var renderChart = function(){
 
-  var imageNames = [];
-  var imageLikes = [];
-  var colors = [];
-  for (var i in allImages){
-    imageNames.push(allImages[i].name);
-    imageLikes.push(allImages[i].likes);
-    colors.push('black');
-  }
+//   var imageNames = [];
+//   var imageLikes = [];
+//   var colors = [];
+//   for (var i in allImages){
+//     imageNames.push(allImages[i].name);
+//     imageLikes.push(allImages[i].likes);
+//     colors.push('black');
+//   }
 
-  var chartData = {
-    labels: imageNames,
-    datasets: [{
-      label: 'Bus Mall Chart',
-      data: imageLikes,
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255,99,132,1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-      ],
-      borderWidth: 1
-    }],
-  };
+//   var chartData = {
+//     labels: imageNames,
+//     datasets: [{
+//       label: 'Bus Mall Chart',
+//       data: imageLikes,
+//       backgroundColor: [
+//         'rgba(255, 99, 132, 0.2)',
+//         'rgba(54, 162, 235, 0.2)',
+//         'rgba(255, 206, 86, 0.2)',
+//         'rgba(75, 192, 192, 0.2)',
+//         'rgba(153, 102, 255, 0.2)',
+//         'rgba(255, 159, 64, 0.2)'
+//       ],
+//       borderColor: [
+//         'rgba(255,99,132,1)',
+//         'rgba(54, 162, 235, 1)',
+//         'rgba(255, 206, 86, 1)',
+//         'rgba(75, 192, 192, 1)',
+//         'rgba(153, 102, 255, 1)',
+//         'rgba(255, 159, 64, 1)'
+//       ],
+//       borderWidth: 1
+//     }],
+//   };
 
-  var chartOptions = {
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero:true
-        }
-      }]
-    },
-    animation: {
-      duration: 1200,
-    },
-    responsive: true,
-  };
+//   var chartOptions = {
+//     scales: {
+//       yAxes: [{
+//         ticks: {
+//           beginAtZero:true
+//         }
+//       }]
+//     },
+//     animation: {
+//       duration: 1200,
+//     },
+//     responsive: true,
+//   };
 
-  var barChart = {
-    type: 'bar',
-    data: chartData,
-    options: chartOptions
-  };
+//   var barChart = {
+//     type: 'horizontalBar',
+//     data: chartData,
+//     options: chartOptions
+//   };
 
 
-  //render the chart
-  var myChart = new Chart(ctx , barChart);
-};
+//   //render the chart
+//   var myChart = new Chart(ctx , barChart);
+// };
